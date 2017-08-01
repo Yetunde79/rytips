a4a5e7d7f7f7218cea05190fa618e45b39694ad3
Yetunde79 <ysolaadebayo@gmail.com>
2017-08-01 17:36:49 -0500

    Add files via upload
    Some of the files are still being worked on

---
+++ google.js
@@ -0,0 +1,7 @@
+<script>
+      window.___gcfg = {
+        lang: 'en-US',
+        parsetags: 'onload'
+      };
+	  </script>
+	   <script src="https://apis.google.com/js/platform.js" async defer></script>
---
+++ search.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+	
+ var a = document.getElementById('tfnewsearch');
+    a.addEventListener('submit',function(e) {
+        e.preventDefault();
+        var b = document.getElementById('tftextinput').value;
+        window.location.href = 'http://mywebsite.com/'+b;
+
+}
